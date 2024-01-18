4686fb07c5d254be2a57d147f13c749783495c8c
ManjiCoder <manjicoder@gmail.com>
2024-01-19 00:19:42 +0530

    test

--- src/pages/api/pdf.js
+++ src/pages/api/pdf.js
@@ -1,6 +1,7 @@
 // pages/api/pdf.js
 
 import pdfParse from 'pdf-parse';
+import { execSync } from 'child_process';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
@@ -13,9 +14,15 @@ export default async (req, res) => {
   const buffer = Buffer.from(pdfData, 'base64');
 
   try {
+    // Use pdftotext to convert PDF to text
+    const textContent = execSync(`pdftotext - -`, { input: buffer });
+
     const data = await pdfParse(buffer);
     // console.log(data.info.Title);
-    res.status(200).json({ text: data.text, numPages: data.numpages });
+    res.status(200).json({
+      text: textContent.toString(),
+      numPages: data.numpages,
+    });
   } catch (error) {
     console.error('Error parsing PDF:', error);
     res.status(500).json({ error: 'Failed to parse PDF.' });
