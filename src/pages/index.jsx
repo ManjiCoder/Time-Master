b2a63ea99cdacf7fda7e6504b00e1cbafae51673
ManjiCoder <manjicoder@gmail.com>
2024-02-01 02:04:51 +0530

    SEO - ✔

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
-import { Baloo_Bhai_2 } from 'next/font/google';
-import ToogleBtn from '@/components/HeadlessUI/ToggleBtn';
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
-import { format } from 'date-fns';
+import { Baloo_Bhai_2 } from 'next/font/google';
+import { persistor } from '@/redux/store';
+import { useDispatch, useSelector } from 'react-redux';
 import { setLogin } from '@/redux/slices/attendanceSlice';
+import { PersistGate } from 'redux-persist/integration/react';
+import { format } from 'date-fns';
+import ToogleBtn from '@/components/HeadlessUI/ToggleBtn';
 import CurrentTimeSpent from '@/components/CurrentTimeSpent';
 import {
   calculateTimeSpent,
@@ -68,81 +70,81 @@ export default function Home() {
     }
   };
   return (
-    // <PersistGate loading={null} persistor={persistor}>
-    <main
-      className={`bg-slate-300 dark:bg-slate-900 dark:text-white text-slate-800 min-h-screen pb-10 ${inter.className}`}
-    >
-      <TimeSpentIndicator isYearMonthPickerVisible={false} />
-      <div className="p-4">
-        <section className="flex justify-between items-center">
-          <h3 className="text-xl font-medium">Welcome </h3>
-          {/* ToggleBtn */}
-          <ToogleBtn />
-        </section>
+    <PersistGate loading={null} persistor={persistor}>
+      <main
+        className={`bg-slate-300 dark:bg-slate-900 dark:text-white text-slate-800 min-h-screen pb-10 ${inter.className}`}
+      >
+        <TimeSpentIndicator isYearMonthPickerVisible={false} />
+        <div className="p-4">
+          <section className="flex justify-between items-center">
+            <h3 className="text-xl font-medium">Welcome </h3>
+            {/* ToggleBtn */}
+            <ToogleBtn />
+          </section>
 
-        <form
-          className="grid grid-cols-2 my-5 gap-3 justify-evenly items-center"
-          onSubmit={handleSubmit}
-        >
-          <div className="time inline-flex flex-col justify-center items-center gap-2 p-4 rounded-md shadow-md bg-slate-200 dark:bg-slate-800">
-            <input
-              className="outline-none focus-within:ring-2 rounded-md shadow-md px-1 py-2 dark:bg-slate-700"
-              type="time"
-              name="login"
-              id="login"
-              // step={300}
-              // pattern="(?:1[012]|0[0-9]):[0-5][0-9] (?:AM|PM)"
-              onChange={(e) => setLoginTime(e.target.value)}
-              placeholder="hh:mm AM/PM"
-              value={loginTime}
-            />
-            <h4 className="text-lg font-medium">Log-In</h4>
-          </div>
-          <div className="time inline-flex flex-col justify-center items-center gap-2 p-4 rounded-md shadow-md bg-slate-200 dark:bg-slate-800">
-            <input
-              className="outline-none focus-within:ring-2 rounded-md shadow-md px-1 py-2 dark:bg-slate-700"
-              type="time"
-              name="logout"
-              id="logout"
-              onChange={(e) => setLogoutTime(e.target.value)}
-              placeholder="hh:mm AM/PM"
-              value={logoutTime}
-            />
-            <h4 className="text-lg font-medium">Log-Out</h4>
-          </div>
-          <div className="pl-3 col-span-2 flex items-center text-sm">
-            <ToggleCheckBox />
-          </div>
-          <button
-            type="submit"
-            className="col-span-2 mx-auto bg-slate-700 px-4 py-2 rounded-md shadow-md text-white"
+          <form
+            className="grid grid-cols-2 my-5 gap-3 justify-evenly items-center"
+            onSubmit={handleSubmit}
           >
-            Submit
-          </button>
-        </form>
+            <div className="time inline-flex flex-col justify-center items-center gap-2 p-4 rounded-md shadow-md bg-slate-200 dark:bg-slate-800">
+              <input
+                className="outline-none focus-within:ring-2 rounded-md shadow-md px-1 py-2 dark:bg-slate-700"
+                type="time"
+                name="login"
+                id="login"
+                // step={300}
+                // pattern="(?:1[012]|0[0-9]):[0-5][0-9] (?:AM|PM)"
+                onChange={(e) => setLoginTime(e.target.value)}
+                placeholder="hh:mm AM/PM"
+                value={loginTime}
+              />
+              <h4 className="text-lg font-medium">Log-In</h4>
+            </div>
+            <div className="time inline-flex flex-col justify-center items-center gap-2 p-4 rounded-md shadow-md bg-slate-200 dark:bg-slate-800">
+              <input
+                className="outline-none focus-within:ring-2 rounded-md shadow-md px-1 py-2 dark:bg-slate-700"
+                type="time"
+                name="logout"
+                id="logout"
+                onChange={(e) => setLogoutTime(e.target.value)}
+                placeholder="hh:mm AM/PM"
+                value={logoutTime}
+              />
+              <h4 className="text-lg font-medium">Log-Out</h4>
+            </div>
+            <div className="pl-3 col-span-2 flex items-center text-sm">
+              <ToggleCheckBox />
+            </div>
+            <button
+              type="submit"
+              className="col-span-2 mx-auto bg-slate-700 px-4 py-2 rounded-md shadow-md text-white"
+            >
+              Submit
+            </button>
+          </form>
 
-        {/* Office Mode */}
+          {/* Office Mode */}
 
-        {isOfficeMode && (
-          <CurrentTimeSpent
-            loginTime={loginTime.trim()}
-            logoutTime={logoutTime.trim()}
-          />
-        )}
-        {/* OnSubmit Mode */}
-
-        {loginTime.trim().length !== 0 &&
-          logoutTime.trim().length !== 0 &&
-          !isOfficeMode && (
+          {isOfficeMode && (
             <CurrentTimeSpent
               loginTime={loginTime.trim()}
               logoutTime={logoutTime.trim()}
             />
           )}
+          {/* OnSubmit Mode */}
 
-        {/* Results */}
-      </div>
-    </main>
-    // </PersistGate>
+          {loginTime.trim().length !== 0 &&
+            logoutTime.trim().length !== 0 &&
+            !isOfficeMode && (
+              <CurrentTimeSpent
+                loginTime={loginTime.trim()}
+                logoutTime={logoutTime.trim()}
+              />
+            )}
+
+          {/* Results */}
+        </div>
+      </main>
+    </PersistGate>
   );
 }
