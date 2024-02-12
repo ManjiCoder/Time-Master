44d625bcf0fce2ef0c75c1a55e52646df9ee7172
Naresh Dhondu <88328872+ManjiCoder@users.noreply.github.com>
2024-02-12 22:26:48 +0530

    Update ExportDataBtn.jsx
    Working

--- src/components/ExportDataBtn.jsx
+++ src/components/ExportDataBtn.jsx
@@ -3,8 +3,9 @@ import { useSelector } from 'react-redux';
 
 const ExportData = () => {
   const attendance = useSelector((state) => state.attendance);
+  const { year, month } = useSelector((state) => state.dateSlice);
 
-  const downloadTxtFile = () => {
+  const downloadJSONFile = () => {
     const jsonData = attendance;
 
     const jsonContent = JSON.stringify(jsonData, null, 2);
@@ -22,10 +23,56 @@ const ExportData = () => {
     URL.revokeObjectURL(url);
   };
 
+  const downloadCSVFile = () => {
+    // // Function to convert JSON to CSV
+    // const jsonToCSV = (data) => {
+    //   const headers = Object.keys(data[0]).join(','); // Extract headers
+
+    //   const rows = data.map((obj) =>
+    //     Object.values(obj)
+    //       .map((val) => `"${val}"`) // Enclose values in double quotes to handle commas in values
+    //       .join(',')
+    //   );
+
+    //   return `${headers}\n${rows.join('\n')}`;
+    // };
+    const jsonData = attendance;
+    let csvTitle = '';
+    let csvDesc = [];
+    const arr = Object.keys(jsonData[year][month])
+      .sort((a, b) => parseInt(a) - parseInt(b))
+      .map((date) => {
+        if (csvTitle !== Object.keys(jsonData[year][month][date])) {
+          csvTitle = Object.keys(jsonData[year][month][date]);
+        }
+
+        csvDesc += Object.values(jsonData[year][month][date]).toString() + '\n';
+      });
+
+    const csvData =
+      csvTitle.map((v) => v.replace(v[0], v[0].toUpperCase())).toString() +
+      '\n' +
+      csvDesc;
+
+    // Download CSV file
+    const blob = new Blob([csvData], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `MasterTime-${month}-${year}.csv`;
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  };
+
   return (
     <button
       className='ml-2 bg-blue-700 px-3 font-bold text-sm py-1.5 rounded-md shadow-md text-white'
-      onClick={downloadTxtFile}
+      onClick={downloadCSVFile}
     >
       {' '}
       Download
@@ -34,3 +81,4 @@ const ExportData = () => {
 };
 
 export default ExportData;
+      
