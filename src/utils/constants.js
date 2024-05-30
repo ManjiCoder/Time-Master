589ddd9d7c6058cbfa4c6c0c8487446a369152ac
Naresh Dhondu <88328872+ManjiCoder@users.noreply.github.com>
2024-05-30 11:26:34 +0530

    Holidays Updated ✔ Election Day Leave added

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ export const holidayDetails = {
   1711305000000: { date: '25-Mar-2024', desc: 'Holi' },
   1712773800000: { date: '11-Apr-2024', desc: 'Ramzan Eid' },
   1714501800000: { date: '01-May-2024', desc: 'Maharashtra Day' },
+  1716143400000: { date: '20-May-2024', desc: 'Election Day' },
   1723660200000: { date: '15-Aug-2024', desc: 'Independence Day' },
   1725647400000: { date: '07-Sep-2024', desc: 'Ganesh Chaturthi' },
   1727807400000: { date: '02-Oct-2024', desc: 'Mahatma Gandhi Jayanti' },
