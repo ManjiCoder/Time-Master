dbbfca385ec0db2d863f9dfd5e9fd464722da71b
ManjiCoder <manjicoder@gmail.com>
2024-06-14 02:24:49 +0530

    Feature Req validation ✔

--- src/components/FeatureReq.jsx
+++ src/components/FeatureReq.jsx
@@ -1,5 +1,6 @@
-import { XMarkIcon } from '@heroicons/react/20/solid';
-import { LightBulbIcon } from '@heroicons/react/24/outline';
+import { msgSchema } from '@/lib/yup';
+import { LightBulbIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Formik } from 'formik';
 import { useState } from 'react';
 import MyModal from './HeadlessUI/Modal';
 
@@ -28,28 +29,65 @@ export default function FeatureReq() {
 
 export function Form({ closeModal }) {
   return (
-    <form className='-mt-3 flex flex-col gap-y-3 px-2 py-3'>
-      <h1 className='flex justify-between text-lg font-bold'>
-        <span className='flex items-center gap-x-1.5'>
-          <LightBulbIcon className='w-7' />
-          Feature Request
-        </span>
-        <button onClick={closeModal}>
-          <XMarkIcon className='w-9 text-gray-900 dark:text-white' />
-        </button>
-      </h1>
-      <textarea
-        cols='30'
-        rows='3'
-        placeholder='Enter Your Message.'
-        className='rounded-md bg-slate-950/40 px-5 py-3 shadow-sm outline-none focus:ring-2'
-      />
-      <button
-        type='submit'
-        className='order-1 col-span-2 mx-auto w-28 rounded-md bg-blue-700 px-4 py-2 font-bold text-white shadow-md dark:bg-blue-500'
-      >
-        Submit
-      </button>
-    </form>
+    <Formik
+      initialValues={{ msg: '' }}
+      validationSchema={msgSchema}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form
+          className='-mt-3 flex flex-col gap-y-3 px-2 py-3 text-gray-900 dark:text-white'
+          onSubmit={handleSubmit}
+        >
+          <h1 className='text-lg font-bold'>
+            <span className='inline-flex items-center gap-x-1.5'>
+              <LightBulbIcon className='w-7' />
+              Feature Request
+            </span>
+            <button onClick={closeModal}>
+              <XMarkIcon className='absolute right-3 top-3 w-9' />
+            </button>
+          </h1>
+          <div className='relative'>
+            <textarea
+              name='msg'
+              cols='30'
+              rows='3'
+              placeholder='Enter Your Message.'
+              className={`rounded-md bg-slate-100 px-5 py-3 shadow-md outline-none focus:ring-2 dark:bg-slate-950/40 ${errors.msg && 'outline-red-600'}`}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.msg}
+            />
+            {errors.msg && touched.msg && (
+              <span className='absolute -bottom-4 right-1 text-xs font-semibold text-red-600'>
+                {errors.msg}
+              </span>
+            )}
+          </div>
+          <button
+            type='submit'
+            disabled={isSubmitting}
+            className='order-1 col-span-2 mx-auto mt-3 w-28 rounded-md bg-blue-700 px-4 py-2 font-bold text-white shadow-md dark:bg-blue-500'
+          >
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
   );
 }
---
+++ src/lib/yup.js
@@ -0,0 +1,8 @@
+import { object, string } from 'yup';
+
+export const msgSchema = object({
+  msg: string()
+    .required('*required')
+    .min(3, 'Should be of minimum 3 characters.')
+    .max(500, 'Should be of maximum 500 characters.'),
+});
