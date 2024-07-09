ad87b9c7bb738963d59b340918618b3a24b47a2b
ManjiCoder <manjicoder@gmail.com>
2024-07-10 01:00:08 +0530

    tanstack query setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,34 +3,39 @@ import DynamicHead from '@/components/DynamicHead';
 import Toastify from '@/components/Toastify';
 import store, { persistor } from '@/redux/store';
 import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'next-themes';
 import { Baloo_Bhai_2 } from 'next/font/google';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 const inter = Baloo_Bhai_2({ subsets: ['latin'] });
-
+const queryClient = new QueryClient();
 export default function App({ Component, pageProps }) {
   return (
-    <main className={inter.className}>
-      <DynamicHead />
-      <Provider store={store}>
-        <PersistGate
-          loading={
+    <QueryClientProvider client={queryClient}>
+      <main className={inter.className}>
+        <DynamicHead />
+        <Provider store={store}>
+          <PersistGate
+            loading={
+              <ThemeProvider attribute='class'>
+                <Component {...pageProps} />
+                <BottomNavbar />
+                <Toastify />
+              </ThemeProvider>
+            }
+            persistor={persistor}
+          >
             <ThemeProvider attribute='class'>
               <Component {...pageProps} />
               <BottomNavbar />
               <Toastify />
             </ThemeProvider>
-          }
-          persistor={persistor}
-        >
-          <ThemeProvider attribute='class'>
-            <Component {...pageProps} />
-            <BottomNavbar />
-            <Toastify />
-          </ThemeProvider>
-        </PersistGate>
-      </Provider>
-    </main>
+          </PersistGate>
+        </Provider>
+      </main>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
