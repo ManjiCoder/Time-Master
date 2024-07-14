32ed5daeff4a803c6a463b3d81997883577d097f
ManjiCoder <manjicoder@gmail.com>
2024-07-14 22:40:15 +0530

    Salary cleanup ✔

--- src/utils/salary.js
+++ src/utils/salary.js
@@ -22,21 +22,6 @@ export const calculateSalary = (
     hoursLeft = 0;
   }
 
-  // const taxRates = {
-  //   January: 200,
-  //   February: 300,
-  //   March: 200,
-  //   April: 200,
-  //   May: 200,
-  //   June: 200,
-  //   July: 200,
-  //   August: 200,
-  //   September: 200,
-  //   October: 200,
-  //   November: 200,
-  //   December: 200,
-  // };
-
   const perDayRate = salaryAmount / days;
   const hourlyRate = salaryAmount / days / hours;
   let salary =
