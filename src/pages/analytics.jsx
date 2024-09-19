e7efabf6c31a5206fb9f4f134b3ef6a91e5c0e14
Manji Coder <manjicoder@gmail.com>
2024-09-20 00:18:22 +0530

    Bar done

--- src/pages/analytics.jsx
+++ src/pages/analytics.jsx
@@ -1,7 +1,13 @@
 import { monthNameToIndex } from '@/utils/dateService';
-import { ArcElement, Chart as ChartJS, PieController, Tooltip } from 'chart.js';
+import {
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  LinearScale,
+  Tooltip,
+} from 'chart.js';
 import { Baloo_Bhai_2 } from 'next/font/google';
-import { Pie } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
 
 const inter = Baloo_Bhai_2({ subsets: ['latin'] });
 
@@ -11,21 +17,21 @@ const data = {
   datasets: [
     {
       label: 'My First Dataset',
-      data: [55, 30, 15, 5],
+      data: [24, 20, 15, 5],
       borderColor: 'rgb(75, 192, 192, 0.6)',
       backgroundColor: ['gold', 'lime', 'purple', 'tomato'],
     },
   ],
 };
 
-ChartJS.register(PieController, ArcElement, Tooltip);
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
 export default function Analytics() {
   return (
     <main
       className={`min-h-screen bg-slate-300 pb-10 text-slate-800 dark:bg-slate-900 dark:text-white ${inter.className} p-4`}
     >
       Analytics
-      <Pie
+      <Bar
         options={{}}
         data={data}
         className='rounded-md p-3 dark:bg-slate-800'
